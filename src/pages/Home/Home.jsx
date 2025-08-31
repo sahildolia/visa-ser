997b02dd4f698db6f605e03766459810bfbf53ef
Jashan dolia <jashandolia70@gmail.com>
2025-08-31 12:56:45 +0530

    Second commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+
+function Home() {
+  return (
+    <>
+    <Navbar/>
+    Home</>
+
+
+  )
+}
+
+export default Home
