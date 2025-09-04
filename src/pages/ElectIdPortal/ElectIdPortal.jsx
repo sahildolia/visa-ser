930946af0f56431240085e0bcf0bf9eebaefec7e
Sahil Dolia <sahil.dolia@sourcemash.com>
2025-09-04 18:29:20 +0530

    error resolved

--- src/pages/ElectIdPortal/ElectIdPortal.jsx
+++ src/pages/ElectIdPortal/ElectIdPortal.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import EidHeader from '../../components/EIDPortal/EidHeader/EidHeader'
+import EidHeader from '../../components/EIDPortal/EidHeader/EidHeader.jsx'
 import EIDforms from '../../components/EIDPortal/EIDforms/EIDforms.jsx'
-import Footer from '../../components/Footer/Footer'
+import Footer from '../../components/Footer/Footer.jsx'
 
 function ElectIdPortal() {
   return (
